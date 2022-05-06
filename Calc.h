62f2f9d835a9f6342de632db3a8e145e1fc37300
officialMartiN <89254143+officialMartiN@users.noreply.github.com>
2022-05-06 16:13:29 +0700

    Create Calc.h

---
+++ Calc.h
@@ -0,0 +1,9 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+
+int sum(int Arr[], int Arrnum);
+int max(int *Arr, int Arrnum);
+int min(int *Arr, int Arrnum);
+int findNum(int *Arr, int Arrnum, int requiredNum);
+
+#endif
