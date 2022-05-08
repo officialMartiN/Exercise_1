87bd78835cebed8da68d1d6f9d8d4483f7bd245e
officialMartiN <89254143+officialMartiN@users.noreply.github.com>
2022-05-08 11:57:06 +0700

    Update m_string.c

--- m_string.c
+++ m_string.c
@@ -14,6 +14,7 @@ int CompareArr(int *Arr1, int *Arr2, int numArr)
 	return 1;
 }
 
+/* Insert one number only
 void InsertArr(int *Arr, int numArr, int insertNum, int insertPos)
 {
 	int newnumArr = numArr + 1;
@@ -35,7 +36,33 @@ void InsertArr(int *Arr, int numArr, int insertNum, int insertPos)
 	}
 	free(newArr);
 }
-
+*/ 
+//Insert Multiple numbers
+void InsertArr (int *mainArr, int main_numArr, int *insertArr, int insert_numArr, int insertPos) 
+{
+	int newnumArr = main_numArr + insert_numArr;
+	int *newArr = malloc (newnumArr * sizeof(int));
+	//Insertion
+	for(int i = 0;i <newnumArr;i++)
+	{
+		if (i < insertPos)
+			newArr[i]= mainArr[i];
+ 		else if (i == insertPos)
+ 		{
+     			for(int j=0; j<insert_numArr; j++)
+    			{
+    				newArr[i+j] = insertArr[j];
+    			}
+     		    	i = i + insert_numArr - 1;
+ 		}
+        	else if (i >= (insertPos + insert_numArr))
+           		newArr[i]= mainArr[i-insert_numArr];
+	}
+	for (int i=0; i<newnumArr;i++)
+	{
+	    printf("%d ",newArr[i]);
+	}
+}
 /*
 int main()
 {
